18b239d5be83d4515db69fc5602c8083176d0836
pacs <pacs@outlook.pt>
2022-09-26 21:51:47 +0100

    finished exercise

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -9,6 +9,10 @@ public class BestLoanRates {
             2, 3.45f,
             3, 2.67f
     );
+
+    public static float getRates(int loanTermInYears) {
+        return 0.0f;
+    }
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name");
