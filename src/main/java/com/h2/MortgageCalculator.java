4b436f1d23702355b448cc1bfa9aa9eb76e0c178
pacs <pacs@outlook.pt>
2022-09-26 23:09:03 +0100

    finished exercise

--- src/main/java/com/h2/MortgageCalculator.java
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -2,7 +2,13 @@ package com.h2;
 
 public class MortgageCalculator {
     private long loanAmount;
-    private int terInYears;
+    private int termInYears;
     private float annualRate;
     private double monthlyPayment;
+
+    public MortgageCalculator( long loanAmount, int termInYears, float annualRate) {
+        this.loanAmount = loanAmount;
+        this.termInYears = termInYears;
+        this.annualRate = annualRate;
+    }
 }
