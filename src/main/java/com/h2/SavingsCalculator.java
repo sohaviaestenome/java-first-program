b29dc589f9ff9f5408b7b514435050c1d16d416d
pacs <pacs@outlook.pt>
2022-09-26 22:14:09 +0100

    finished exercise

--- src/main/java/com/h2/SavingsCalculator.java
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -10,6 +10,10 @@ public class SavingsCalculator {
     }
 
     private float sumOfCredits() {
-        return 0.0f;
+        float sum = 0.0f;
+
+        for ( int i=0; i< credits.length; i++) {
+            sum += credits[i];
+        }
     }
 }
