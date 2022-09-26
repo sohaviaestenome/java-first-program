77bc4be0e534dec9735145276e5066f53bcd68fc
pacs <pacs@outlook.pt>
2022-09-26 21:50:23 +0100

    finished exercise

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -1,7 +1,19 @@
 package com.h2;
+import java.util.Map;
+import java.util.Scanner;
 
 public class BestLoanRates {
+
+    public static final Map<Integer, Float> bestRates = Map.of(
+            1, 5.50f,
+            2, 3.45f,
+            3, 2.67f
+    );
     public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = scanner.nextLine();
+        System.out.println("Hello " + name);
 
     }
 }
