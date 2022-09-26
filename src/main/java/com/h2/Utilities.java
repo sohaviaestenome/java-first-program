3563624e2527ee69c20808f3f8a57d061d16ff28
pacs <pacs@outlook.pt>
2022-09-26 23:45:13 +0100

    finished exercise

---
+++ src/main/java/com/h2/Utilities.java
@@ -0,0 +1,15 @@
+package com.h2;
+
+public class Utilities {
+
+    public static long getLongValue(String in) {
+        long out = Long.MIN_VALUE;
+
+        try {
+            out = Long.parseLong(in);
+        }catch(NumberFormatException e) {
+           throw new IllegalArgumentException(in + " cannot be converted into a 'long' value. Exiting program." );
+        }
+        return out;
+    }
+}
