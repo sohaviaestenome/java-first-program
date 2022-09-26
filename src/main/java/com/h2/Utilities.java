c4399eda67fc1027077c7d84cf810c2eced91ef3
pacs <pacs@outlook.pt>
2022-09-26 23:49:57 +0100

    finished exercise

--- src/main/java/com/h2/Utilities.java
+++ src/main/java/com/h2/Utilities.java
@@ -19,7 +19,7 @@ public class Utilities {
         try {
             out = Integer.parseInt(in);
         }catch(NumberFormatException e) {
-            throw new IllegalArgumentException(in + " cannot be converted into a 'int' value. Exiting program.")
+            throw new IllegalArgumentException(in + " cannot be converted into a 'int' value. Exiting program.");
         }
         return out;
     }
