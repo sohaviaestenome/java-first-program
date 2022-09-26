162ac5e0c42094952a5ded08278aa6d86b118356
pacs <pacs@outlook.pt>
2022-09-26 22:08:50 +0100

    finished exercise

--- src/main/java/com/h2/SavingsCalculator.java
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -3,4 +3,8 @@ package com.h2;
 public class SavingsCalculator {
     private float[] credits;
     private float[] debits;
+
+    public SavingsCalculator(float[] credits, float[] debits ) {
+
+    }
 }
